5fb892fd1930b7a5f7235820cd947ca81efa872e
raacker <haven.cpp@gmail.com>
2017-04-16 01:36:31 +0900

    run in background

--- app/src/main/java/com/raacker/wwbaalarm/AlarmMainActivity.java
+++ app/src/main/java/com/raacker/wwbaalarm/AlarmMainActivity.java
@@ -1,14 +1,13 @@
 package com.raacker.wwbaalarm;
 
-import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,15 +17,16 @@ import android.widget.ToggleButton;
 
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Map;
 
 public class AlarmMainActivity extends AppCompatActivity implements View.OnClickListener {
-    public static final String TAG = "MainActivity";
+    public static final String TAG = "AlarmMainActivity";
     public static final long INTERVAL_TIME = 24 * 60 * 60 * 1000;
 
     private TimePickerFragment timePickerFragment;
     private AlarmManager alarmManager;
     private ToggleButton monday, tuesday, wednesday, thursday, friday, saturday, sunday;
+    private MediaPlayer mediaPlayer;
+    private boolean isPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +41,26 @@ public class AlarmMainActivity extends AppCompatActivity implements View.OnClick
         friday = (ToggleButton) findViewById(R.id.toggle_fri);
         saturday = (ToggleButton) findViewById(R.id.toggle_sat);
         sunday = (ToggleButton) findViewById(R.id.toggle_sun);
-        Button set_alarm = (Button) findViewById(R.id.set_alarm);
-        Button set_time = (Button) findViewById(R.id.set_time);
+        Button setAlarm = (Button) findViewById(R.id.set_alarm);
+        Button setTime = (Button) findViewById(R.id.set_time);
+        Button playManually = (Button) findViewById(R.id.play_manually);
         timePickerFragment = new TimePickerFragment();
-        set_alarm.setOnClickListener(this);
-        set_time.setOnClickListener(this);
+        setAlarm.setOnClickListener(this);
+        setTime.setOnClickListener(this);
+        playManually.setOnClickListener(this);
+        try{
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(getResources().getString(R.string.url));
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setScreenOnWhilePlaying(true);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
     }
 
     @Override
@@ -57,6 +71,9 @@ public class AlarmMainActivity extends AppCompatActivity implements View.OnClick
                 break;
             case R.id.set_alarm:
                 registerAlarm(this);
+                break;
+            case R.id.play_manually:
+                playRadioManually();
                 break;
         }
     }
@@ -72,7 +89,13 @@ public class AlarmMainActivity extends AppCompatActivity implements View.OnClick
         intent.putExtra("weekday", week);
         PendingIntent pendingIntent = getPendingIntent(intent);
         long alarmTime = getTime();
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, alarmTime, INTERVAL_TIME, pendingIntent);
+        ComponentName receiver = new ComponentName(context, AlarmReceiver.class);
+        PackageManager pm = context.getPackageManager();
+
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, alarmTime, INTERVAL_TIME, pendingIntent);
     }
 
     private PendingIntent getPendingIntent(Intent intent) {
@@ -101,6 +124,21 @@ public class AlarmMainActivity extends AppCompatActivity implements View.OnClick
         return alarmTime;
     }
 
+    private void playRadioManually() {
+        try{
+            mediaPlayer.prepare();
+            if(isPlaying) {
+                isPlaying = false;
+                mediaPlayer.pause();
+            } else {
+                isPlaying = true;
+                mediaPlayer.start();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+ /*
     private void saveAlarm() {
         SharedPreferences pref = getSharedPreferences("alarm_list", Activity.MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
@@ -110,7 +148,6 @@ public class AlarmMainActivity extends AppCompatActivity implements View.OnClick
     private void loadAlarm() {
         SharedPreferences pref = getSharedPreferences("alarm_list", Activity.MODE_PRIVATE);
         Map<String, ?> stringSet = pref.getAll();
-
-
     }
+*/
 }
--- app/src/main/java/com/raacker/wwbaalarm/AlarmReceiver.java
+++ app/src/main/java/com/raacker/wwbaalarm/AlarmReceiver.java
@@ -4,37 +4,59 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.PowerManager;
 import android.util.Log;
 
 import java.io.IOException;
 import java.util.Calendar;
 
-public class AlarmReceiver extends BroadcastReceiver{
+public class AlarmReceiver extends BroadcastReceiver {
     public static final String TAG = "AlarmReceiver";
-
-    @Override
+    private MediaPlayer mediaPlayer;
+    private PowerManager powerManager;
+    private PowerManager.WakeLock wakeLock;
+    //@Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive() called");
-        MediaPlayer mediaPlayer;
         boolean[] week = intent.getBooleanArrayExtra("weekday");
+        powerManager = ((PowerManager)context.getSystemService(Context.POWER_SERVICE));
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "alarmStart");
 
+        wakeLock.acquire();
         Calendar cal = Calendar.getInstance();
         if(week[cal.get(Calendar.DAY_OF_WEEK)]){
             Log.d(TAG, "Alarm called");
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
 
-            try{
+                @Override
+                public void run() {
+                    PlaySync play = new PlaySync();
+                    play.execute();
+                }
+            }, 10000);
+        }
+    }
+
+    private class PlaySync extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            try {
                 mediaPlayer = new MediaPlayer();
-                mediaPlayer.setDataSource(context.getResources().getString(R.string.url));
+                mediaPlayer.setDataSource("https://purdue.streamguys1.com/wbaa-news");
                 mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                //mediaPlayer.setWakeMode(context, PowerManager.);
                 mediaPlayer.setScreenOnWhilePlaying(true);
                 mediaPlayer.prepare();
                 mediaPlayer.start();
+                if(wakeLock.isHeld())
+                    wakeLock.release();
             } catch(IOException e) {
-               // Toast.makeText(get, "Some problem caused while streaming", Toast.LENGTH_SHORT);
                 e.printStackTrace();
             }
+            return null;
         }
     }
 }
